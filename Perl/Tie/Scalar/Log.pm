72d144c04c93769d85f3e4e2249502e9f847c9da
Dean_Kreger <justin.nami.lee@gmail.com>
2017-12-04 13:44:11 -0400

    Update Log.pm -- Updating License

--- Perl/Tie/Scalar/Log.pm
+++ Perl/Tie/Scalar/Log.pm
@@ -4,11 +4,11 @@ package Tie::Scalar::Log;
 
 # Tie::Scalar::Log.pm:
 #
-# Copyright (C) 2013  Justin Lee  < justin at taiz dot me >
+# Copyright (C) 2013-2017  Justin Lee  < justin at taiz dot me >
 #
 # This program is free software; you can redistribute it and/or
-# modify it under the terms of the GNU General Public License version 2
-# as published by the Free Software Foundation.
+# modify it under the terms of the GNU General Public License version 3
+# or higher as published by the Free Software Foundation.
 #
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
